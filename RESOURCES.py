5d7edad2278300709d25a4306051fc6bcbf0476f
ndem <nathan.demaestri@gmail.com>
2020-05-24 12:43:44 -0400

    Move resources in dedicated file

---
+++ RESOURCES.py
@@ -0,0 +1,22 @@
+RESOURCES = [
+    {
+        'ref': 'NBC_MONTHLY_ECONOMIC',
+        'name': 'National bank monthly economic monitor',
+        'url': 'https://www.nbc.ca/content/dam/bnc/en/rates-and-analysis/economic-analysis/monthly-economic-monitor.pdf',
+    },
+    {
+        'ref': 'NBC_MONTHLY_EQUITY',
+        'name': 'National bank monthly equity monitor',
+        'url': 'https://www.nbc.ca/content/dam/bnc/en/rates-and-analysis/economic-analysis/monthly-equity-monitor.pdf',
+    },
+    {
+        'ref': 'NBC_MONTHLY_FIXED_INCOME',
+        'name': 'National bank monthly fixed income monitor',
+        'url': 'https://www.nbc.ca/content/dam/bnc/en/rates-and-analysis/economic-analysis/monthly-fixed-income-monitor.pdf',
+    },
+    {
+        'ref': 'NBC_MONTHLY_FOREX',
+        'name': 'National bank monthly economic monitor',
+        'url': 'https://www.nbc.ca/content/dam/bnc/en/rates-and-analysis/economic-analysis/forex.pdf',
+    },
+]
--- retriever.py
+++ retriever.py
@@ -2,32 +2,10 @@ import hashlib
 import logging
 import requests
 from requests.exceptions import RequestException
+
+from RESOURCES import RESOURCES
 from utils.emailing import email_alert
 from utils.checksum import get_checksum, create_checksum
-
-
-RESOURCES = [
-    {
-        'ref': 'NBC_MONTHLY_ECONOMIC',
-        'name': 'National bank monthly economic monitor',
-        'url': 'https://www.nbc.ca/content/dam/bnc/en/rates-and-analysis/economic-analysis/monthly-economic-monitor.pdf',
-    },
-    {
-        'ref': 'NBC_MONTHLY_EQUITY',
-        'name': 'National bank monthly equity monitor',
-        'url': 'https://www.nbc.ca/content/dam/bnc/en/rates-and-analysis/economic-analysis/monthly-equity-monitor.pdf',
-    },
-    {
-        'ref': 'NBC_MONTHLY_FIXED_INCOME',
-        'name': 'National bank monthly fixed income monitor',
-        'url': 'https://www.nbc.ca/content/dam/bnc/en/rates-and-analysis/economic-analysis/monthly-fixed-income-monitor.pdf',
-    },
-    {
-        'ref': 'NBC_MONTHLY_FOREX',
-        'name': 'National bank monthly economic monitor',
-        'url': 'https://www.nbc.ca/content/dam/bnc/en/rates-and-analysis/economic-analysis/forex.pdf',
-    },
-]
 
 
 def main():
